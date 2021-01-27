c68881740fafe0b83393bf10ef6d3b4cc1326657
Niklas Kiefer <niklas.kiefer@camunda.com>
2021-01-27 15:59:47 +0100

    chore: migrate to rollup@2

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,23 @@
 import pkg from './package.json';
 
-import cjs from 'rollup-plugin-commonjs';
+import cjs from '@rollup/plugin-commonjs';
 
-import babel from 'rollup-plugin-babel';
+import { babel } from '@rollup/plugin-babel';
+
+import nodeResolve from '@rollup/plugin-node-resolve';
+
+import replace from '@rollup/plugin-replace';
 
 function pgl() {
   return [
     cjs(),
+    nodeResolve(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify( 'production' )
+    }),
     babel({
       babelrc: false,
-      runtimeHelpers: true,
+      babelHelpers: 'runtime',
       exclude: 'node_modules/**',
       presets: [
         ['@babel/env', { modules: false }],
